43a1c4ce5d8c31d3c0b1a941a2c886bb0114a40d
mr012420 <remi.mathieu@etu.unice.fr>
2024-06-26 16:03:29 +0200

    amelioration css et js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const elements = document.querySelectorAll('.animate__animated');
+
+    elements.forEach(element => {
+        element.classList.add('animate__animated');
+    });
+});
