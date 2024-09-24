103c00049e9a3e79d93c7dd05c29698328553736
Remi Mathieu <remi.mathieu@etu.unice.fr>
2024-09-24 08:47:32 +0200

    TD2 Carto Commit 2

--- Cours/BUT3/R506/CartoTD2/Exo1/script.js
+++ Cours/BUT3/R506/CartoTD2/Exo1/script.js
@@ -1,6 +1,5 @@
-var map = L.map('map').setView([43.7, 7.26], 13); // Vue sur Nice par défaut
+var map = L.map('map').setView(navigator.geolocation);
 
-    // Ajout du fond de carte OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);
@@ -8,16 +7,13 @@ var map = L.map('map').setView([43.7, 7.26], 13); // Vue sur Nice par défaut
     // Marqueur pour Nice
     var niceMarker = L.marker([43.7, 7.26]).addTo(map).bindPopup("Nice, Centre Ville");
 
-    // Récupérer la position de l'utilisateur
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var userLat = position.coords.latitude;
             var userLng = position.coords.longitude;
 
-            // Centrer la carte sur la position de l'utilisateur
             map.setView([userLat, userLng], 13);
 
-            // Ajouter un marqueur sur la position de l'utilisateur
             L.marker([userLat, userLng]).addTo(map).bindPopup("Votre position").openPopup();
         });
     } else {
