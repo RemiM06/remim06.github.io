24a4e9da2c4dd436ed0410be48cbbe593174057c
Remi Mathieu <remi.mathieu@etu.unice.fr>
2024-09-24 09:46:29 +0200

    TD2 ex2

--- Cours/BUT3/R506/CartoTD2/Exo2/script.js
+++ Cours/BUT3/R506/CartoTD2/Exo2/script.js
@@ -2,8 +2,8 @@
 var map = L.map('map').setView([25.774, -80.19], 4);
 
 // Ajout de la carte Stadia Maps
-L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png', {
-    attribution: '<a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>Stadia Maps</a>, <a href="https://www.stamen.com/" target="_blank">Stamen Design</a>, <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>, <a href="https://www.openstreetmap.org/copyright/" target="_blank">OpenStreetMap</a>'
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=VOTRE_CLE_API', {
+    attribution: '<a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, <a href="https://www.stamen.com/" target="_blank">Stamen Design</a>, <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a>, <a href="https://www.openstreetmap.org/copyright/" target="_blank">OpenStreetMap</a>'
 }).addTo(map);
 
 // Définition du Triangle des Bermudes
@@ -15,6 +15,14 @@ var bermudaTriangle = L.polygon([
     color: 'red'
 }).addTo(map).bindPopup("Triangle des Bermudes");
 
+// Positions de Marseille et Nice
+var marseille = [43.3, 5.4];
+var nice = [43.7, 7.26];
+
+// Ajout de marqueurs pour Marseille et Nice
+L.marker(marseille).addTo(map).bindPopup("Marseille");
+L.marker(nice).addTo(map).bindPopup("Nice");
+
 // Géolocalisation de l'utilisateur
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -23,16 +31,18 @@ if (navigator.geolocation) {
         var accuracy = position.coords.accuracy;
 
         // Ajout d'un marqueur pour la position de l'utilisateur et d'un cercle de précision
-        L.marker([userLat, userLng]).addTo(map).bindPopup("Votre position").openPopup();
+        var userMarker = L.marker([userLat, userLng]).addTo(map).bindPopup("Votre position");
+        userMarker.openPopup();
         L.circle([userLat, userLng], { radius: accuracy }).addTo(map);
 
-        // Marqueurs entre Marseille et Nice, calcul et affichage de la distance
-        var marseille = [43.3, 5.4];
-        var nice = [43.7, 7.26];
+        // Segment entre Marseille et Nice
+        L.polyline([marseille, nice], { color: 'blue' }).addTo(map).bindPopup("Segment entre Marseille et Nice");
 
         // Calcul de la distance entre Marseille et la position de l'utilisateur
-        var distance = map.distance(marseille, [userLat, userLng]) / 1000;
-        L.polyline([marseille, nice], { color: 'blue' }).addTo(map).bindPopup("Marseille à Nice = " + distance.toFixed(2) + "km" );
+        var distanceToMarseille = map.distance(marseille, [userLat, userLng]) / 1000; // Distance en kilomètres
+        
+        // Ajout d'un marqueur pour afficher la distance à Marseille
+        userMarker.bindPopup("Votre position<br>Distance à Marseille : " + distanceToMarseille.toFixed(2) + " km").openPopup();
     });
 } else {
     alert("Géolocalisation non supportée par votre navigateur.");
